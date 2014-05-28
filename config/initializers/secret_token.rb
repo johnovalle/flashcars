d39df23c55a0c88d5732c1eb5c460a70255453a5
kugyousha <johnovalle@gmail.com>
2014-05-28 17:14:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flashcars::Application.config.secret_key_base = 'c1fec44e646a63799a9e15f83fd075998c2759e78ae68c93ece1e190457f112e60516939dafa3d2aa1a595582a986cfd5c003465ddd6d18b918ceee0c77347ce'
